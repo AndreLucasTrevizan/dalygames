1dc56e288db17cca0ddd32d75c031e28f4f7e20c
André Lucas Trevizan <trevizan.al@gmail.com>
2025-08-20 17:16:06 -0300

    DalyGames

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,84 @@
-export default function Home() {
+import { Container } from "@/components/container";
+import { GameCard } from "@/components/gameCard";
+import { Input } from "@/components/input";
+import { GameProps } from "@/utils/types/game";
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRightSquare } from 'react-icons/bs';
+
+async function getDalyGame() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {
+      next: {
+        revalidate: 320
+      }
+    });
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+async function getGamesData() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, {
+      next: {
+        revalidate: 320
+      }
+    });
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GameProps = await getDalyGame();
+  const games: GameProps[] = await getGamesData();
+
   return (
-    <main className="flex">
-      <h1>Teste</h1>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você</h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-4">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority
+                quality={100}
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+              />
+            </div>
+          </section>
+        </Link>
+        <Input />
+
+        <h2
+          className="text-lg font-bold mt-8 mb-5"
+        >Jogos para conhecer</h2>
+
+        <section
+          className="
+            grid
+            gap-7
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+          "
+        >
+          {games.map((game) => (
+            <GameCard key={game.id} game={game} />
+          ))}
+        </section>
+      </Container>
     </main>
   );
 }
---
+++ src/components/input/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { FiSearch } from 'react-icons/fi';
+
+import { useRouter } from 'next/navigation';
+
+export function Input() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+    if (input == "") {
+      return;
+    }
+
+    //Roteamento imperativo
+    router.push(`/game/search/${input}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="w-full bg-slate-200 my-5 flex gap-2 items-center justify-between rounded-lg p-2"
+    >
+      <input
+      className="bg-slate-200 outline-none w-11/12"
+        type="text"
+        placeholder="Procurando algum jogo?..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">
+        <FiSearch size={24} color="#EA580C" />
+      </button>
+    </form>
+  );
+}
